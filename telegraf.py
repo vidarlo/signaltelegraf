0e40ff6dcab6c082cb9c3037efc9f78d390b3522
Vidar Løkken <vidarlo@bitsex.net>
2024-10-06 11:17:11 +0200

    Add resend logic on unmanned

--- telegraf.py
+++ telegraf.py
@@ -3,22 +3,31 @@
 import socket
 import threading
 
-def send_signal(dst_ip, dst_port=5919):
+designator=b'GAR' # This stations identification. Three bytes.
+receivefrom=[ b'THU', b'GAR'] # Our neighbouring stations
+unmanned=True
+
+def send_signal(dst_ip, data, dst_port=5919, magic=b'GVB'):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.sendto(bytes("PLING", "utf-8"), (dst_ip, dst_port))
+    s.sendto(magic+data, (dst_ip, dst_port))
     s.close()
 
-def recv_signal(bind_ip='0.0.0.0', bind_port=5919):
+def recv_signal(bind_ip='0.0.0.0', bind_port=5919, magic=b'GVB'):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.bind((bind_ip, bind_port))
     while True:
         data, addr = s.recvfrom(1024)
-        print("Received: %s" % data)
+        if data[0:3] == magic and data[3:6] in receivefrom:
+            print("Packet from %s" % data[3:6])
+            if unmanned and data[6:9] != designator:
+                send_signal(dst_ip='127.0.0.1', data=designator + data[3:6])
+                print("Resent")
+
 
 def get_input():
     while True:
         input("Press enter")
-        send_signal('127.0.0.1')
+        send_signal('127.0.0.1', designator)
 
 if __name__ == "__main__":
     receiver = threading.Thread(target=recv_signal)
